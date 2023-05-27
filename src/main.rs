4f4d7e40d41f8b90d4f25bee60c13f9476fd4a71
michabay05 <michabay05@gmail.com>
2023-05-27 17:56:55 -0400

    Added eval constants

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,17 @@ mod bb;
 mod board;
 mod consts;
 mod engine;
+mod eval;
+mod eval_consts;
 mod fen;
 mod magic_consts;
 mod magics;
 mod move_gen;
 mod moves;
 mod perft;
-mod perft_test;
 
 use std::env;
 
 fn main() {
-    let args = env::args().collect::<Vec<String>>();
-    perft_test::test(args);
-    // engine::test();
+    engine::test();
 }
