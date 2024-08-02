f002899f7d37b9227a7bfb18036379316188a802
michabay05 <michabay05@gmail.com>
2024-08-02 18:36:31 -0400

    Flipped squares([A8..H1] to [A1..H8]) and added bitboard shifting

--- src/bb.rs
+++ src/bb.rs
@@ -1,3 +1,4 @@
+use crate::consts::{Direction, File, MASK_FILE};
 use crate::SQ;
 
 pub type BB = u64;
@@ -6,25 +7,36 @@ pub trait BBUtil {
     fn set(&mut self, ind: usize);
     fn get(&self, ind: usize) -> bool;
     fn pop(&mut self, ind: usize);
+    fn toggle(&mut self, ind: usize);
     fn lsb(&self) -> usize;
     fn pop_lsb(&mut self) -> usize;
+    fn shift(&mut self, dir: Direction);
     #[allow(dead_code)]
     fn print(&self);
 }
 
 impl BBUtil for BB {
+    #[inline(always)]
     fn set(&mut self, ind: usize) {
         *self |= 1 << ind;
     }
 
+    #[inline(always)]
     fn get(&self, ind: usize) -> bool {
         *self & (1 << ind) > 0
     }
 
+    #[inline(always)]
     fn pop(&mut self, ind: usize) {
+        *self &= !(1 << ind);
+    }
+
+    #[inline(always)]
+    fn toggle(&mut self, ind: usize) {
         *self ^= 1 << ind;
     }
 
+    #[inline(always)]
     fn lsb(&self) -> usize {
         let num = *self ^ (*self - 1);
         let count = num.count_ones() as usize;
@@ -34,10 +46,31 @@ impl BBUtil for BB {
         count - 1
     }
 
+    #[inline(always)]
     fn pop_lsb(&mut self) -> usize {
         let ind = self.lsb();
         self.pop(ind);
         ind
+    }
+
+    #[inline(always)]
+    fn shift(&mut self, dir: Direction) {
+        *self = match dir {
+            Direction::North => *self << 8,
+            Direction::South => *self >> 8,
+            Direction::NorthNorth => *self << 16,
+            Direction::SouthSouth => *self >> 16,
+            Direction::East => (*self & !MASK_FILE[File::H as usize]) << 1,
+            Direction::West => (*self & !MASK_FILE[File::A as usize]) >> 1,
+            Direction::Northeast => (*self & !MASK_FILE[File::H as usize]) << 9,
+            Direction::Northwest => (*self & !MASK_FILE[File::A as usize]) << 7,
+            Direction::Southeast => (*self & !MASK_FILE[File::H as usize]) >> 7,
+            Direction::Southwest => (*self & !MASK_FILE[File::A as usize]) >> 9,
+            _ => {
+                eprintln!("Unhandled bitboard shifting direction");
+                *self
+            },
+        };
     }
 
     fn print(&self) {
@@ -50,5 +83,63 @@ impl BBUtil for BB {
         }
         println!("     - - - - - - - -");
         println!("     a b c d e f g h");
+    }
+}
+
+#[cfg(test)]
+mod bb_tests {
+    use crate::consts::{Direction, PieceColor, Sq};
+
+    use super::{BBUtil, BB};
+
+    #[test]
+    fn test_shift() {
+        let mut bb: BB;
+        let mut expected: BB;
+
+        use Direction::*;
+        use PieceColor::*;
+        use Sq::*;
+
+        let arr = [
+            (Light, E2, NorthNorth, E4),
+            (Light, E7, SouthSouth, E5),
+            (Light, E4, North, E5),
+            (Light, E4, South, E3),
+            (Light, E4, East, F4),
+            (Light, E4, West, D4),
+            (Light, H4, West, G4),
+            (Light, H7, North, H8),
+
+            (Dark, C7, NorthNorth, C5),
+            (Dark, H2, SouthSouth, H4),
+            (Dark, E5, North, E4),
+            (Dark, E5, South, E6),
+            (Dark, E5, East, D5),
+            (Dark, E5, West, F5),
+            (Dark, B4, East, A4),
+            (Dark, H8, North, H7),
+            (Dark, B6, Northeast, A5),
+            (Dark, A2, Southwest, B3),
+        ];
+
+        for (side, sq, dir, shifted) in arr {
+            bb = 0;
+            expected = 0;
+            bb.set(sq as usize);
+            bb.shift(dir.relative(side));
+            expected.set(shifted as usize);
+            if bb != expected {
+                eprintln!(
+                    "[{:?}] {} shifted {:?} = {} (expected = {})",
+                    side,
+                    sq,
+                    dir,
+                    Sq::from_num(bb.lsb()),
+                    Sq::from_num(expected.lsb())
+                );
+                assert!(false);
+            }
+        }
     }
 }
--- src/consts.rs
+++ src/consts.rs
@@ -1,7 +1,6 @@
 #![allow(dead_code)]
 
 use std::fmt;
-use crate::bb::BB;
 
 #[macro_export]
 macro_rules! SQ {
@@ -32,11 +31,21 @@ macro_rules! FLIP_SQ {
     };
 }
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum PieceColor {
     Light,
     Dark,
     Both,
+}
+
+impl PieceColor {
+    pub fn opposite(self) -> Self {
+        if self == Self::Light {
+            Self::Dark
+        } else {
+            Self::Light
+        }
+    }
 }
 
 #[derive(Copy, Clone, PartialEq)]
@@ -96,6 +105,39 @@ impl Piece {
         Some(piece)
     }
 
+    pub fn from_type(color: PieceColor, pt: PieceType) -> Self {
+        match color {
+            PieceColor::Light => match pt {
+                PieceType::Pawn => Self::LP,
+                PieceType::Knight => Self::LN,
+                PieceType::Bishop => Self::LB,
+                PieceType::Rook => Self::LR,
+                PieceType::Queen => Self::LQ,
+                PieceType::King => Self::LK
+            }
+            PieceColor::Dark => match pt {
+                PieceType::Pawn => Self::DP,
+                PieceType::Knight => Self::DN,
+                PieceType::Bishop => Self::DB,
+                PieceType::Rook => Self::DR,
+                PieceType::Queen => Self::DQ,
+                PieceType::King => Self::DK
+            }
+            PieceColor::Both => unreachable!("Shouldn't be here!"),
+        }
+    }
+
+    pub fn piece_type(&self) -> PieceType {
+        match *self {
+            Self::LP | Self::DP => PieceType::Pawn,
+            Self::LN | Self::DN => PieceType::Knight,
+            Self::LB | Self::DB => PieceType::Bishop,
+            Self::LR | Self::DR => PieceType::Rook,
+            Self::LQ | Self::DQ => PieceType::Queen,
+            Self::LK | Self::DK => PieceType::King,
+        }
+    }
+
     pub fn from_char(piece_char: char) -> Option<Self> {
         assert!(PIECE_CHAR.into_iter().any(|x| x == piece_char));
         let char_index = PIECE_CHAR
@@ -126,16 +168,19 @@ impl Piece {
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
+#[repr(i32)]
 pub enum Direction {
     North = 8,
     South = -8,
     West = -1,
     East = 1,
-    NE = 9,     // NORTH + EAST
-    NW = 7,     // NORTH + WEST
-    SE = -7,    // SOUTH + EAST
-    SW = -9,    // SOUTH + WEST
+    Northeast = 9,     // NORTH + EAST
+    Northwest = 7,     // NORTH + WEST
+    Southeast = -7,    // SOUTH + EAST
+    Southwest = -9,    // SOUTH + WEST
+    NorthNorth = 16,
+    SouthSouth = -16,
     NE_N = 17,  // 2(NORTH) + EAST -> 'KNIGHT ONLY'
     NE_E = 10,  // NORTH + 2(EAST) -> 'KNIGHT ONLY'
     NW_N = 15,  // 2(NORTH) + WEST -> 'KNIGHT ONLY'
@@ -146,17 +191,51 @@ pub enum Direction {
     SW_W = -10, // SOUTH + 2(WEST) -> 'KNIGHT ONLY'
 }
 
+impl Direction {
+    pub fn relative(&self, side: PieceColor) -> Self {
+        if side == PieceColor::Light {
+            return *self;
+        }
+        match *self {
+            Self::North => Self::South,
+            Self::South => Self::North,
+            Self::East => Self::West,
+            Self::West => Self::East,
+            Self::NorthNorth => Self::SouthSouth,
+            Self::SouthSouth => Self::NorthNorth,
+            Self::Northeast => Self::Southwest,
+            Self::Northwest => Self::Southeast,
+            Self::Southeast => Self::Northwest,
+            Self::Southwest => Self::Northeast,
+            Self::NE_E => Self::SE_E,
+            Self::NE_N => Self::SE_S,
+            Self::NW_W => Self::SW_W,
+            Self::NW_N => Self::SW_S,
+            Self::SE_E => Self::NE_E,
+            Self::SE_S => Self::NE_N,
+            Self::SW_W => Self::NW_W,
+            Self::SW_S => Self::NW_N,
+        }
+    }
+}
+
+#[rustfmt::skip]
+#[derive(Copy, Clone, PartialEq)]
+pub enum File {
+    A, B, C, D, E, F, G, H
+}
+
 #[rustfmt::skip]
 #[derive(Copy, Clone, PartialEq)]
 pub enum Sq {
-    A8, B8, C8, D8, E8, F8, G8, H8,
-    A7, B7, C7, D7, E7, F7, G7, H7,
-    A6, B6, C6, D6, E6, F6, G6, H6,
-    A5, B5, C5, D5, E5, F5, G5, H5,
-    A4, B4, C4, D4, E4, F4, G4, H4,
-    A3, B3, C3, D3, E3, F3, G3, H3,
-    A2, B2, C2, D2, E2, F2, G2, H2,
     A1, B1, C1, D1, E1, F1, G1, H1,
+    A2, B2, C2, D2, E2, F2, G2, H2,
+    A3, B3, C3, D3, E3, F3, G3, H3,
+    A4, B4, C4, D4, E4, F4, G4, H4,
+    A5, B5, C5, D5, E5, F5, G5, H5,
+    A6, B6, C6, D6, E6, F6, G6, H6,
+    A7, B7, C7, D7, E7, F7, G7, H7,
+    A8, B8, C8, D8, E8, F8, G8, H8
 }
 
 impl fmt::Display for Sq {
@@ -177,70 +256,70 @@ impl Sq {
 
     pub fn from_num(sq_num: usize) -> Self {
         match sq_num {
-            0 => Self::A8,
-            1 => Self::B8,
-            2 => Self::C8,
-            3 => Self::D8,
-            4 => Self::E8,
-            5 => Self::F8,
-            6 => Self::G8,
-            7 => Self::H8,
-            8 => Self::A7,
-            9 => Self::B7,
-            10 => Self::C7,
-            11 => Self::D7,
-            12 => Self::E7,
-            13 => Self::F7,
-            14 => Self::G7,
-            15 => Self::H7,
-            16 => Self::A6,
-            17 => Self::B6,
-            18 => Self::C6,
-            19 => Self::D6,
-            20 => Self::E6,
-            21 => Self::F6,
-            22 => Self::G6,
-            23 => Self::H6,
-            24 => Self::A5,
-            25 => Self::B5,
-            26 => Self::C5,
-            27 => Self::D5,
-            28 => Self::E5,
-            29 => Self::F5,
-            30 => Self::G5,
-            31 => Self::H5,
-            32 => Self::A4,
-            33 => Self::B4,
-            34 => Self::C4,
-            35 => Self::D4,
-            36 => Self::E4,
-            37 => Self::F4,
-            38 => Self::G4,
-            39 => Self::H4,
-            40 => Self::A3,
-            41 => Self::B3,
-            42 => Self::C3,
-            43 => Self::D3,
-            44 => Self::E3,
-            45 => Self::F3,
-            46 => Self::G3,
-            47 => Self::H3,
-            48 => Self::A2,
-            49 => Self::B2,
-            50 => Self::C2,
-            51 => Self::D2,
-            52 => Self::E2,
-            53 => Self::F2,
-            54 => Self::G2,
-            55 => Self::H2,
-            56 => Self::A1,
-            57 => Self::B1,
-            58 => Self::C1,
-            59 => Self::D1,
-            60 => Self::E1,
-            61 => Self::F1,
-            62 => Self::G1,
-            63 => Self::H1,
+            0 => Self::A1,
+            1 => Self::B1,
+            2 => Self::C1,
+            3 => Self::D1,
+            4 => Self::E1,
+            5 => Self::F1,
+            6 => Self::G1,
+            7 => Self::H1,
+            8 => Self::A2,
+            9 => Self::B2,
+            10 => Self::C2,
+            11 => Self::D2,
+            12 => Self::E2,
+            13 => Self::F2,
+            14 => Self::G2,
+            15 => Self::H2,
+            16 => Self::A3,
+            17 => Self::B3,
+            18 => Self::C3,
+            19 => Self::D3,
+            20 => Self::E3,
+            21 => Self::F3,
+            22 => Self::G3,
+            23 => Self::H3,
+            24 => Self::A4,
+            25 => Self::B4,
+            26 => Self::C4,
+            27 => Self::D4,
+            28 => Self::E4,
+            29 => Self::F4,
+            30 => Self::G4,
+            31 => Self::H4,
+            32 => Self::A5,
+            33 => Self::B5,
+            34 => Self::C5,
+            35 => Self::D5,
+            36 => Self::E5,
+            37 => Self::F5,
+            38 => Self::G5,
+            39 => Self::H5,
+            40 => Self::A6,
+            41 => Self::B6,
+            42 => Self::C6,
+            43 => Self::D6,
+            44 => Self::E6,
+            45 => Self::F6,
+            46 => Self::G6,
+            47 => Self::H6,
+            48 => Self::A7,
+            49 => Self::B7,
+            50 => Self::C7,
+            51 => Self::D7,
+            52 => Self::E7,
+            53 => Self::F7,
+            54 => Self::G7,
+            55 => Self::H7,
+            56 => Self::A8,
+            57 => Self::B8,
+            58 => Self::C8,
+            59 => Self::D8,
+            60 => Self::E8,
+            61 => Self::F8,
+            62 => Self::G8,
+            63 => Self::H8,
             _ => unreachable!("Sq::from_num() should only contain these values: [0-63]"),
         }
     }
@@ -252,16 +331,28 @@ impl Sq {
 
 pub use SQ;
 
+use crate::bb::BB;
+
 #[rustfmt::skip]
 const PIECE_CHAR: [char; 13] = ['P', 'N', 'B', 'R', 'Q', 'K', 'p', 'n', 'b', 'r', 'q', 'k', ' '];
 #[rustfmt::skip]
-const STR_COORDS: [&str; 65] = [
-    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
-    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
-    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
-    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
-    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
-    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+const STR_COORDS: [&str; 64] = [
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
     "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
-    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", " "
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+];
+
+pub const MASK_FILE: [BB; 8] = [
+    0x101010101010101, 0x202020202020202, 0x404040404040404, 0x808080808080808,
+    0x1010101010101010, 0x2020202020202020, 0x4040404040404040, 0x8080808080808080,
+];
+
+pub const MASK_RANK: [BB; 8] = [
+    0xff, 0xff00, 0xff0000, 0xff000000,
+    0xff00000000, 0xff0000000000, 0xff000000000000, 0xff00000000000000
 ];
